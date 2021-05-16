098c970667fd14912d985f12f0d42c4e85d6e2e5
Soreing <Soreing.dev@gmail.com>
2021-05-16 04:00:53 +0400

    Added include Guard to directions

--- src/directions.h
+++ src/directions.h
@@ -1,1 +1,6 @@
+#ifndef DIRECTIONS_H
+#define DIRECTIONS_H
+
 enum Direction{ UP, DOWN, LEFT, RIGHT };
+
+#endif
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,21 +1,17 @@
 #include "graphics.h"
 #include <iostream>
 
-// Sets the value of the global OutputHandle
-// And hides the console cursor to create a black canvas
 void init()
 {   outputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO params{1, FALSE};
     SetConsoleCursorInfo(outputHandle, &params);
 }
 
-// Draws a 1-2 character string on the console at coordinates x and y
 void draw(int x, int y, int* arr)
 {   SetConsoleCursorPosition(outputHandle, COORD{(short)(x*2), (short)y});
     std::cout<< (char*)arr;
 }
 
-// Erases 2 characters at coordinates x and y on the console
 void erase(short x, short y)
 {   SetConsoleCursorPosition(outputHandle, COORD{(short)(x*2), (short)y});
     std::cout<< "  ";
